05780699cda932a6d6811962b28001b29fbc5879
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-08-19 23:43:13 -0300

    Incluindo Spring data

--- osworks-api/src/main/java/com/nobuwebdeveloper/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/nobuwebdeveloper/algaworks/osworks/api/controller/ClienteController.java
@@ -1,32 +1,27 @@
 package com.nobuwebdeveloper.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nobuwebdeveloper.algaworks.osworks.domain.model.Cliente;
+import com.nobuwebdeveloper.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
+
 	
+	@Autowired
+	private ClienteRepository ClienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("15 0000-1111");
-		cliente1.setEmail("joaodascouves@algaworks.com");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("15 2222-1111");
-		cliente2.setEmail("Maria@algaworks.com");
-		
-		return Arrays.asList(cliente1, cliente2);
-		
+		return ClienteRepository.findAll();
 	}
 
 }
