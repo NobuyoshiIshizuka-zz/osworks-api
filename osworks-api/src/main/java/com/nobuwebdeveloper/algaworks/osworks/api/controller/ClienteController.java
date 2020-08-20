72a228715e179085de7e2a0ffb00f35808e4fe30
Nobuyoshi Ishizuka <nobuyoshi_japa@hotmail.com>
2020-08-20 00:10:01 -0300

    Create method buscar

--- osworks-api/src/main/java/com/nobuwebdeveloper/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/nobuwebdeveloper/algaworks/osworks/api/controller/ClienteController.java
@@ -1,12 +1,12 @@
 package com.nobuwebdeveloper.algaworks.osworks.api.controller;
 
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nobuwebdeveloper.algaworks.osworks.domain.model.Cliente;
@@ -21,7 +21,19 @@ public class ClienteController {
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
+		//return ClienteRepository.findAll();
 		return ClienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = ClienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
